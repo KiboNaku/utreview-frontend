eda43527f6a0297e144bd7625a2b8336d728e61d
zhangirisz <zhangirisz@gmail.com>
2020-05-22 15:08:35 -0500

    Test bug on iris computer

--- utflow/src/App.js
+++ utflow/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <MainContent />
+        <Footer />
+      </div>
+    );
+  }
+  
 }
 
 export default App;
