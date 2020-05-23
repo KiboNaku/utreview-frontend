8b5403e7be581bd3d48245da8cf2b23d5db4bc8d
zhangirisz <zhangirisz@gmail.com>
2020-05-22 20:13:02 -0500

    Revert "Pull changes"
    This reverts commit 68bf70a7b58d7301bfdd54f279df7b05ace1df53, reversing
    changes made to ba990e3f456c2b8ba0f10479d3444b326cbdbb20.

--- utflow/src/App.js
+++ utflow/src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <h1> Iris Zhang </h1>
+      <p> EE student </p>
+      <ul>
+        <li> Japan </li>
+        <li> Japan </li>
+        <li> Japan </li>
+      </ul>
     </div>
   );
 }
