dfe0bb406a8d1e80a972d7ed61f81f8541c7cc9b
Andy Ni <nakukibo@gmail.com>
2020-05-25 12:39:53 -0500

    about skeleton

---
+++ utflow/src/pages/about/About.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import Contributor from './Contributor'
+
+class Footer extends Component {
+
+    render() {
+
+        return (
+
+            <main>
+                <Contributor/>
+                <Contributor/>
+                <Contributor/>
+            </main>
+        )
+    }
+}
+
+export default Footer
