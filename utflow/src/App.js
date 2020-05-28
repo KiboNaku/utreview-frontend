f9ba89e612c9e889d88c87e2370204419573360c
zhangirisz <zhangirisz@gmail.com>
2020-05-28 15:21:31 -0500

    Added react-router-dom

--- utflow/src/App.js
+++ utflow/src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import NavBar from './pages/_components/NavBar'
+import Home from './pages/home/Home'
+import Popup from './pages/popups/Popup'
+import {Link, Route, Switch} from 'react-router-dom';
 
 function App() {
 	return (
 		<div className="App">
+			<NavBar showSearch="false"/>
+			<Switch>
+				<Route exact path="/" component={Home} />
+				<Route path="/login" component ={Popup} />
+			</Switch>
 		</div>
 	);
 }
