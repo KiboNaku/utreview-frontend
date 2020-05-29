325d1caf12d7ef0a11c70219e94f13ee7885df94
zhangirisz <zhangirisz@gmail.com>
2020-05-28 19:25:34 -0500

    Added basic routing functionality

--- utflow/src/pages/_components/NavBar.js
+++ utflow/src/pages/_components/NavBar.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import SearchBar from './SearchBar'
 import {Link, Route, Switch} from 'react-router-dom';
-import "./NavBar.css"
 
 class NavBar extends Component {
 
--- utflow/src/pages/details/course/CourseAddReview.js
+++ utflow/src/pages/details/course/CourseAddReview.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { withRouter, Link } from 'react-router-dom'
 import './CourseDetails.css'
 
 function CourseAddReview(props) {
-	return (
-		<div className="courseAddReview" >
+    return (
+        <div className="courseAddReview" >
             <p> Have you taken {props.courseDep} {props.courseNo}? </p>
-            <button className="btn btn-primary" type="button">
-                Add a Review
-            </button>	
-		</div>
-	);
+            <Link to="/add-review">
+                <button className="btn btn-primary" type="button">
+                    Add a Review
+                </button>
+            </Link>
+
+        </div>
+    );
 }
 
-export default CourseAddReview;
+export default withRouter(CourseAddReview);
