3f89c69f61d8feed4633443a5507d3639782e613
vinaxue <43913920+vinaxue@users.noreply.github.com>
2020-05-30 18:49:31 -0500

    added dropdown search for prof name and course num

---
+++ utflow/src/pages/review/ReviewFormFunctions.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+const testData = [
+	"EE302", "EE306"
+]
+
+export function getCourseNum() {
+	// axios.get('api/get-course-num')
+	// .then((response) => {
+	//   // do something get those numbers 
+	// });
+
+	return testData
+}
+
+export function getProfessorNames() {
+	// axios.get('api/get-prof-name')
+	// 	.then((response) => {
+	// 		// do something get those numbers 
+	// 	});
+}
