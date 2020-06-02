121cad26340792f29ab8da1cce2427b04fb4f13d
Andy Ni <nakukibo@gmail.com>
2020-06-01 20:24:14 -0500

    updated home page design

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,16 +1,24 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import NavBar from './../_components/NavBar'
 import SearchBar from './../_components/SearchBar'
 import Footer from './../_components/Footer'
 import './home.css'
 
 class Home extends Component {
-    render(){
+    render() {
         return (
             <div>
                 <main>
-                    <div className="col-sm-12 col-md-8 col-lg-6 col-xl-6">
-                        <SearchBar/>
+                    <div className="container text-center">
+                        
+                        <div>
+                            <h1>What starts here ...</h1>
+                            <h1>changes your course schedule</h1>
+                        </div>
+                        
+                        <br/>
+                        
+                        <SearchBar />
                     </div>
                 </main>
             </div>
