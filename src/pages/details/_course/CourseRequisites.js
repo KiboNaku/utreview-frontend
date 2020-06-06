c314218b480b332b2de3a68100c149d167e46257
Iris Zhang <zhangirisz@gmail.com>
2020-06-06 15:48:53 -0500

    Added course schedule to course details page

--- src/pages/details/_course/CourseRequisites.js
+++ src/pages/details/_course/CourseRequisites.js
@@ -2,38 +2,41 @@ import React from 'react';
 import './../CourseDetails.css'
 
 function CourseRequisites(props) {
-    const prerequisites = props.preRequisites.map( prereq =>{
+    const prerequisites = props.preRequisites.map(prereq => {
         return <li> {prereq} </li>
     })
-    const corequisites = props.coRequisites.map( coreq =>{
+    const corequisites = props.coRequisites.map(coreq => {
         return <li> {coreq} </li>
     })
-    const antirequisites = props.antiRequisites.map( antireq =>{
+    const antirequisites = props.antiRequisites.map(antireq => {
         return <li> {antireq} </li>
     })
-	return (
-		<div className="courseRequisites">
-			<h1> Requisites </h1>	
-            <div>
-                <h5> Prerequisites </h5>
-                <ul>
-                    {prerequisites}
-                </ul>
+    return (
+        <div className="courseRequisites">
+            <h1> Requisites </h1>
+            <div className="d-flex">
+                <div>
+                    <h5> Prerequisites </h5>
+                    <ul>
+                        {prerequisites}
+                    </ul>
+                </div>
+                <div>
+                    <h5> Corequisites </h5>
+                    <ul>
+                        {corequisites}
+                    </ul>
+                </div>
+                <div>
+                    <h5> Antirequisites </h5>
+                    <ul>
+                        {antirequisites}
+                    </ul>
+                </div>
             </div>
-            <div>
-                <h5> Corequisites </h5>
-                <ul>
-                    {corequisites}
-                </ul>
-            </div>
-            <div>
-                <h5> Antirequisites </h5>
-                <ul>
-                    {antirequisites}
-                </ul>
-            </div>			
-		</div>
-	);
+
+        </div>
+    );
 }
 
 export default CourseRequisites;
