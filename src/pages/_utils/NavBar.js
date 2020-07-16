d46a6d0bc582d93a33c81e4e9687eaab9eaff474
Andy Ni <nakukibo@gmail.com>
2020-07-15 19:38:23 -0500

    NavBar: implemented circle dropdown

--- src/pages/_utils/NavBar.js
+++ src/pages/_utils/NavBar.js
@@ -20,7 +20,31 @@ class NavBar extends Component {
         )
 
         const logout = (
-            <button type="button" className="btn font-weight-bold btn-nav" onClick={this.logOut.bind(this)}>Log out</button>
+            // <div class="dropdown-menu-left">
+            //     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            //         <img class="rounded-circle nav-bar-profile" alt="100x100" src={require("./../../res/img/default.jpg")}
+            //             data-holder-rendered="true" />
+            //     </a>
+            //     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            //         <a class="dropdown-item" href="#">Action</a>
+            //         <a class="dropdown-item" href="#">Another action</a>
+            //         <div class="dropdown-divider"></div>
+            //         <a class="dropdown-item" href="#">Something else here</a>
+            //     </div>
+            // </div>
+            <li class="nav-item dropdown d-inline-block">
+                <a data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">
+                    <img class="rounded-circle nav-bar-profile" alt="100x100" src={require("./../../res/img/default.jpg")}
+                        data-holder-rendered="true" />
+                </a>
+                <div aria-labelledby="navbarDropdownMenuLink" className="dropdown-menu dropdown-menu-right">
+                    <Link className="dropdown-item" to="/profile">
+                        Profile
+                    </Link>
+                    <div class="dropdown-divider"></div>
+                    <span className="dropdown-item clickable" onClick={this.logOut.bind(this)}>Log Out</span>
+                </div>
+            </li>
         )
 
         const signup = (
@@ -31,7 +55,7 @@ class NavBar extends Component {
             <Link to="/profile">
                 <button type="button" className="btn font-weight-bold btn-nav">Profile</button>
             </Link>
-            
+
         )
 
         /**
@@ -54,17 +78,22 @@ class NavBar extends Component {
                     <nav className="navbar navbar-expand-lg navbar-dark transparent py-3 justify-content-between invisible">
 
                         <div className="navbar-brand" className={"font-weight-bold float-left align-self-center" + (showSearch && " d-none d-md-block")}>
-                            <Link id="nav-brand" className="utdark px-2" to="/">UT Review</Link>
+                            <Link className={(!showSearch && "big-brand-txt ") + "px-2"} to="/">
+                                UT Review
+                            </Link>
                         </div>
 
                         <div className={showSearch ? "search-wrapper mr-auto" : "d-none"} >
                             <SearchBar />
                         </div>
-
                         <div className="ml-auto">
-                            <span className="align-self-center mr-2 d-none d-md-inline">
-                                {localStorage.usertoken ? profile : signup}
-                            </span>
+
+
+                            {!localStorage.usertoken &&
+                                <span className="align-self-center mr-2 d-none d-md-inline">
+                                    {signup}
+                                </span>
+                            }
 
                             <span className="align-self-center mr-2">
                                 {localStorage.usertoken ? logout : login}
@@ -74,8 +103,8 @@ class NavBar extends Component {
                     </nav>
                 }
 
-                <nav className={"navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar py-3 justify-content-between " 
-                                + (transparent? "transparent": "bg-dark")}>
+                <nav className={"navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar py-3 justify-content-between "
+                    + (transparent ? "transparent" : "bg-dark")}>
 
                     <div className="navbar-brand" className={"font-weight-bold float-left align-self-center" + (showSearch && " d-none d-md-block")}>
                         <Link className={(!showSearch && "big-brand-txt ") + "px-2"} to="/">
@@ -86,11 +115,14 @@ class NavBar extends Component {
                     <div className={showSearch ? "search-wrapper mr-auto" : "d-none"} >
                         <SearchBar />
                     </div>
-
                     <div className="ml-auto">
-                        <span className="align-self-center mr-2 d-none d-md-inline">
-                            {localStorage.usertoken ? profile : signup}
-                        </span>
+
+
+                        {!localStorage.usertoken &&
+                            <span className="align-self-center mr-2 d-none d-md-inline">
+                                {signup}
+                            </span>
+                        }
 
                         <span className="align-self-center mr-2">
                             {localStorage.usertoken ? logout : login}
