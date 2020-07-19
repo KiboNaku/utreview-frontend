a6bc10a69592a386464cd8ec648ffc069fadca38
cw-create <colinwang2001@gmail.com>
2020-07-18 22:17:32 -0400

    completed the updated profile page review summaries. changed it into a scrollable window.

--- src/pages/profile/_utils/ReviewSummary.js
+++ src/pages/profile/_utils/ReviewSummary.js
@@ -18,54 +18,52 @@ function ReviewSummary(props) {
 
     return (
         <div className="review-container">
-            <div className="card-wrap">
-                <div className="card-body">   
-                    <Confirm
-                        title="Delete Review"
-                        message="Are you sure you want to delete this review?"
-                        handleOk={() => props.deleteReview(props.data.id)}
-                    />
-                    <p className="course-name-wrapper" 
-                    style={props.data.courseRating.approval ? {color: "green"} : {color: "red"}}>
-                        <span className="course-name">{props.data.course.dept.abr} {props.data.course.num}</span>
-                    </p>
+            <div className="review-wrapper"> 
+                <Confirm
+                    title="Delete Review"
+                    message="Are you sure you want to delete this review?"
+                    handleOk={() => props.deleteReview(props.data.id)}
+                />
+                <p className="course-name-wrapper" 
+                style={props.data.courseRating.approval ? {color: "#a6cd57"} : {color: "#ed7f7b"}}>
+                    <span className="course-name">{props.data.course.dept.abr} {props.data.course.num}</span>
+                </p>
 
-                    <p className="prof-name" 
-                    style={props.data.profRating.approval ? {color: "green"} : {color: "red"}}>
-                        <span>{props.data.prof.firstName} {props.data.prof.lastName}</span>
-                    </p>
+                <p className="prof-name" 
+                style={props.data.profRating.approval ? {color: "#a6cd57"} : {color: "#ed7f7b"}}>
+                    <span>{props.data.prof.firstName} {props.data.prof.lastName}</span>
+                </p>
 
-                    <p className="semester-date">
-                        <span>{props.data.semester.semester} {props.data.semester.year}</span>
-                    </p>
-    
-                    <p className="update-date"><small>Last updated: {props.data.date}</small></p>
+                <p className="semester-date">
+                    <span>{props.data.semester.semester} {props.data.semester.year}</span>
+                </p>
 
-                    <div className="delete-review">
-                        <div data-toggle="modal" data-target="#confirmModal">Delete</div>
-                    </div>
+                <p className="update-date"><small>Last updated: {props.data.date}</small></p>
 
-                    <ReviewDetails
-                        data={props.data}
-                        editReview={props.editReview}
-                    />
-                    <div className="button-wrapper1">
-                        <button type="button"
-                            className="review-buttons"
-                            data-toggle="modal"
-                            data-target={"#review-details-modal" + props.data.id}
-                        > More Details
-                        </button>
-                    </div>
-                    <br></br>
-                    <div className="button-wrapper2"> 
-                        <button
-                            type="button"
-                            className="review-buttons"
-                            onClick={() => props.editReview(props.data.id)}
-                        > Edit Review
-                        </button>
-                    </div>
+                <ReviewDetails
+                    data={props.data}
+                    editReview={props.editReview}
+                />
+                <div className="button-wrapper1">
+                    <button type="button"
+                        className="review-buttons"
+                        data-toggle="modal"
+                        data-target={"#review-details-modal" + props.data.id}
+                    > More Details
+                    </button>
+                </div>
+                <br></br>
+                <div className="button-wrapper2"> 
+                    <button
+                        type="button"
+                        className="review-buttons"
+                        onClick={() => props.editReview(props.data.id)}
+                    > Edit Review
+                    </button>
+                </div>
+
+                <div className="delete-review">
+                    <div data-toggle="modal" data-target="#confirmModal">Delete</div>
                 </div>
             </div>
         </div>
