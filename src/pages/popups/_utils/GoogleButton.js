f9bf845425f9b409540f73e4abbcff1a0cb51ba9
vinaxue <43913920+vinaxue@users.noreply.github.com>
2020-07-21 17:48:43 -0500

    google sign in

--- src/pages/popups/_utils/GoogleButton.js
+++ src/pages/popups/_utils/GoogleButton.js
@@ -3,10 +3,11 @@ import React from 'react'
 
 function GoogleButton(props) {
     return (
-        <button className="btn btn-lg btn-dark btn-block font-weight-bold" type="submit">
-            <span className="fab fa-google px-3"></span>
-            <span>{props.text}</span>
-        </button>
+        // <button className="btn btn-lg btn-dark btn-block font-weight-bold" type="submit">
+        //     <span className="fab fa-google px-3"></span>
+        //     <span>{props.text}</span>
+        // </button>
+        <div class="g-signin2" data-onsuccess="onSignIn"></div>
     )
 }
 
