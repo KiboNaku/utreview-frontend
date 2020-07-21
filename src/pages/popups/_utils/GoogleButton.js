76df7ee425acc28fbe1d8f5b68835a8e9abbbbec
vinaxue <43913920+vinaxue@users.noreply.github.com>
2020-07-21 18:25:34 -0500

    google sign in

--- src/pages/popups/_utils/GoogleButton.js
+++ src/pages/popups/_utils/GoogleButton.js
@@ -7,7 +7,7 @@ function GoogleButton(props) {
         //     <span className="fab fa-google px-3"></span>
         //     <span>{props.text}</span>
         // </button>
-        <div class="g-signin2" data-onsuccess="onSignIn"></div>
+        <div className="g-signin2" data-onsuccess="onSignIn"></div>
     )
 }
 
