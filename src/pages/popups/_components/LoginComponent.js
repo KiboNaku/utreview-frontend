de30499e9ff510ea021377033083378df670284a
Iris Zhang <zhangirisz@gmail.com>
2020-07-26 16:57:04 -0500

    Integrate formik and yup into login form

--- src/pages/popups/_components/LoginComponent.js
+++ src/pages/popups/_components/LoginComponent.js
@@ -1,11 +1,130 @@
 import React from 'react'
+import axios from 'axios'
+import * as Yup from 'yup'
+import { useFormik, Field, Formik, Form, ErrorMessage, getIn } from 'formik'
 import GoogleButton from "./../_utils/GoogleButton"
 import ModalHeader from "./../_utils/ModalHeader"
 import UTEmail from "./../_utils/UTEmail"
 import './../../../utcolors.css'
 import Loading from './../../_utils/Loading'
 
+function invalidInputStyle(errors, touched, fieldName) {
+    if (getIn(errors, fieldName) && getIn(touched, fieldName)) {
+        return {
+            border: '1px solid red'
+        }
+    }else{
+        return null
+    }
+}
+
 function LoginComponent(props) {
+
+    const LoginForm = () => {
+        return (
+            <Formik
+                initialValues={{
+                    email: '',
+                    password: '',
+                }}
+                validateOnChange={false}
+                validateOnBlur={false}
+                validationSchema={Yup.object({
+                    email: Yup.string()
+                        .test('Check valid email', 'An account does not exist for this email',
+                            function (value) {
+                                if(value === undefined){
+                                    return true
+                                }
+                                return new Promise((resolve, reject) => {
+                                    axios
+                                        .post('/api/check_valid_email', {
+                                            email: value + '@utexas.edu',
+                                        })
+                                        .then(response => {
+                                            if (response.data.error) {
+                                                resolve(false)
+                                            } else {
+                                                resolve(true)
+                                            }
+                                        })
+                                })
+                            }
+                        )
+                        .required('Required'),
+                    password: Yup.string()
+                        .test('Check valid password', 'Invalid email/password combination',
+                            function (value) {
+                                if(value === undefined){
+                                    return true
+                                }
+                                return new Promise((resolve, reject) => {
+                                    axios
+                                        .post('/api/check_valid_password', {
+                                            email: this.parent.email + '@utexas.edu',
+                                            password: value
+                                        })
+                                        .then(response => {
+                                            if (response.data.error) {
+                                                resolve(false)
+                                            } else {
+                                                resolve(true)
+                                            }
+                                        })
+                                })
+                            }
+                        )
+                        .required('Required')
+                })}
+                onSubmit={(values, { validate }) => {
+                    props.onSubmit(values)
+                }}
+            >
+                {formik => {
+
+                    return (
+                        <form onSubmit={formik.handleSubmit}>
+                            <div className="my-3">
+                                <div className="form-label-group">
+                                    <label htmlFor="email">Email</label>
+                                    <span className="d-flex">
+                                        <Field
+                                            name="email"
+                                            type="text"
+                                            className="form-control d-inline"
+                                            placeholder="john.doe"
+                                            style={invalidInputStyle(formik.errors, formik.touched, 'email')}
+                                        />
+                                        <label className="px-2 float-right" style={{ marginTop: 6 }}>@utexas.edu</label>
+                                    </span>
+                                    <ErrorMessage component="div" className="text-danger" name="email" />
+                                </div>
+                            </div>
+
+                            <div className="form-group my-3">
+                                <label htmlFor="password">Password</label>
+                                <Field
+                                    name="password"
+                                    type="password"
+                                    className="form-control"
+                                    style={invalidInputStyle(formik.errors, formik.touched, 'password')}
+                                />
+                                <ErrorMessage component="div" className="text-danger" name="password" />
+                            </div>
+
+                            <span className="forgot-password-text center" type="button" data-toggle="modal" data-target="#forgot-password-modal">
+                                    <a data-dismiss="modal" data-toggle="modal" data-target="#forgot-password-modal" className="utcolor"><p className="center">Forgot Password?</p></a>
+                                </span>
+
+                            <button className="btn btn-lg btn-utcolor btn-block mt-2 font-weight-bold" type="submit"> Log in </button>
+
+                        </form>
+                    )
+                }}
+
+            </Formik>
+        )
+    }
 
     let loading =
         <div className="on-top">
@@ -23,35 +142,8 @@ function LoginComponent(props) {
                     <div className="modal-body">
 
                         {props.data.loading && loading}
-                        <form className="form-signin mt-3" onSubmit={props.onSubmit}>
 
-                            <div className="my-3">
-                                <UTEmail email={props.data.email} onChange={props.onChange} />
-                            </div>
-
-                            <div className="form-group my-3">
-                                <input
-                                    type="password"
-                                    name="password"
-                                    className="form-control"
-                                    value={props.data.password}
-                                    onChange={props.onChange}
-                                    placeholder="password"
-                                    required />
-                            </div>
-
-                            {/* <div className="form-label-group checkbox my-1">
-                                <input type="checkbox" value="remember-me" />
-                                <label className="pl-2">Remember me</label>
-                            </div> */}
-
-                            <button className="btn btn-lg btn-utcolor btn-block mt-2 font-weight-bold" type="submit">Log In</button>
-
-                            <span className="forgot-password-text center" type="button" data-toggle="modal" data-target="#forgot-password-modal">
-                                    <a data-dismiss="modal" data-toggle="modal" data-target="#forgot-password-modal" className="utcolor"><p className="center">Forgot Password?</p></a>
-                                </span>
-
-                        </form>
+                        <LoginForm />
 
                         <div className="text-center my-3">
                             <h5><strong>OR</strong></h5>
