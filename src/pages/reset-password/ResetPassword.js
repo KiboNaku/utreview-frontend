3e17bc486fb4d7c46fbe614cd8b30c023a30536c
Andy Ni <nakukibo@gmail.com>
2020-07-29 16:58:59 -0500

    ResetPassword: adding loading screen

--- src/pages/reset-password/ResetPassword.js
+++ src/pages/reset-password/ResetPassword.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { withRouter, Link, Redirect } from 'react-router-dom'
 import ResetPasswordForm from './ResetPasswordForm'
-import Loading from './../_utils/Loading'   
+import Loading from './../_utils/Loading'
 import axios from 'axios'
 import qs from 'qs'
 
@@ -11,6 +11,7 @@ class ResetPassword extends Component {
 
         super()
         this.state = {
+            loading: false,
             redirect: false,
             success: 0,
             error: null,
@@ -40,27 +41,33 @@ class ResetPassword extends Component {
         this.setState({ [e.target.name]: e.target.value })
     }
 
-    onSubmit(e){
+    onSubmit(e) {
         e.preventDefault()
-        if(this.state.password !== this.state.confirmPassword){
-            this.setState({passwordsNoMatch: true})
-        }else{
-            this.setState({passwordsNoMatch: false})
+
+        if (this.state.password !== this.state.confirmPassword) {
+            this.setState({ passwordsNoMatch: true })
+        } else {
+            this.setState({ passwordsNoMatch: false, loading: true })
             axios
-            .post('/api/reset_password', {
-                email: localStorage.email,
-                password: this.state.password
-            })
-            .then(response => {
-                this.setState({passwordUpdated: true})
-            })
+                .post('/api/reset_password', {
+                    email: localStorage.email,
+                    password: this.state.password
+                })
+                .then(response => {
+                    this.setState({ passwordUpdated: true, loading: false })
+                })
         }
     }
-    
+
 
     render() {
 
         let message = ""
+
+        let loading =
+            <div className="on-top">
+                <Loading />
+            </div>
 
         if (this.state.success < 0) {
             message = "An error has occured: " + this.state.error
@@ -68,14 +75,16 @@ class ResetPassword extends Component {
 
         let successMessage = "Your password has been successfully updated"
 
-        let resetForm = <ResetPasswordForm onSubmit={this.onSubmit} onChange={this.onChange} data={this.state}/>
+        let resetForm = <ResetPasswordForm onSubmit={this.onSubmit} onChange={this.onChange} data={this.state} />
 
         return (
             <main className="bg-grey">
                 <div className="main-sub container py-5">
                     <div className="container justify-content-center px-5 py-5 col-12 col-sm-11 col-md-9 col-lg-7 bg-light">
                         <h3 className='py-5 text-center'>
-                            {this.state.passwordUpdated ? successMessage: (this.state.success < 0 ? message : resetForm)}
+
+                            {this.state.loading && loading}
+                            {this.state.passwordUpdated ? successMessage : (this.state.success < 0 ? message : resetForm)}
                         </h3>
                     </div>
                 </div>
