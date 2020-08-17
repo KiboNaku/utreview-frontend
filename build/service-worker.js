0d9fc7b0d9f1f626a24e714cbee4f0d0fe03deea
Andy Ni <nakukibo@gmail.com>
2020-08-16 20:11:39 -0500

    reverted build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.755a46d253c741e32a7000600712a1be.js"
+  "/precache-manifest.bc71c305340dad230f09725279937c7f.js"
 );
 
 self.addEventListener('message', (event) => {
