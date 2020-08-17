d78a7bfe3e4612002bca0ac9ffbefe8bd9591d05
Andy Ni <nakukibo@gmail.com>
2020-08-16 19:57:31 -0500

    Updated build

--- src/pages/details/_course/CourseInfo/CourseInfo.js
+++ src/pages/details/_course/CourseInfo/CourseInfo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import './CourseInfo.css'
-import './UserRatings.css'
+import './UserRatings.scss'
 
 function CourseInfo(props) {
 
