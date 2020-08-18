a1b46ffeabf672d559bb52556ad12ea3f111b443
cw-create <colinwang2001@gmail.com>
2020-08-17 22:47:11 -0400

    More changes to course details

--- src/pages/details/_course/CourseReviews/CourseAddReview.js
+++ src/pages/details/_course/CourseReviews/CourseAddReview.js
@@ -24,22 +24,22 @@ function CourseAddReview(props) {
                 }
             }}
         >
-            <button style={{ height: "50px", width: "175px", fontSize: "20px" }} className="btn btn-dark font-weight-bold" type="button">
-                Add a Review
+            <button  className="add-review-button" type="button">
+                <span className="add-review-button-text">Add a Review</span>
             </button>
         </Link>
     )
 
     const loginLink = (
         <div>
-            <button style={{ height: "50px", width: "175px", fontSize: "20px" }} className="btn btn-dark font-weight-bold" type="button" data-toggle="modal" data-target="#login-modal">
+            <button className="add-review-button" type="button" data-toggle="modal" data-target="#login-modal">
                 Add a Review
             </button>
         </div>
     )
     
     return (
-        <div className="courseAddReview" >
+        <div className="courseAddReview">
             <h3 className="add-review-text"> Have you taken {props.courseDept} {props.courseNum}? </h3>
             <div className="add-review">
                 {localStorage.usertoken ? addReviewLink : loginLink}
